6d7b4a51b594f9e12fae4f8ee6bc418cfa3b2ed9
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-09-09 00:43:04 +0800

    learning databases.

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Item(models.Model):
+    TodoList = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    complete = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.text
+
+
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>My Project</h1>")
+# def index(response):
+#     return HttpResponse("<h1>My Project</h1>")
 
-def v1(response):
-    return HttpResponse("<h1>view 1!</h1>")
+# def v1(response):
+#     return HttpResponse("<h1>view 1!</h1>")
+
+def index(response, name):
+    ls = TodoList.objects.get(name=name)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(items.text)))
